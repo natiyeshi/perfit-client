293f739d33215e8c41e03442f0f5fcb1764836e5
Natnael Yeshiwas <natiyeshi16@gmail.com>
2025-02-08 17:52:29 +0300

    AA

--- src/app/dashboard/_modules/import/page.tsx
+++ src/app/dashboard/_modules/import/page.tsx
@@ -2,7 +2,6 @@
 
 import ImportTable from "./_components/ImportTable";
 const Page = () => {
-  // redirect("/dashboard/admin/hr/employees");
   return (
     <div className="overflow-y-auto w-full h-full flex-1   flex flex-col">
       <ImportTable />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/providers/Providers";
-const defaultUrl = process.env.VERCEL_URL
-? `https://${process.env.VERCEL_URL}`
-: "http://localhost:3000";
 
 const inter = Inter({ subsets: ["latin"] });
 
