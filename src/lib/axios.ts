03ca6654221f39ab0a692ba043ebb3b6fdd2869a
Natnael Yeshiwas <natiyeshi16@gmail.com>
2025-02-08 19:46:40 +0300

    deploy works

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const localApi = "http://localhost:4040/api/v1/"
-const mainApi = "https://perfit-erp.onrender.com/api/v1/"
+const mainApi = "http://51.20.191.52/api/v1/"
 
 const instance = axios.create({
-  baseURL: localApi,
+  baseURL: mainApi,
 });
 
 
